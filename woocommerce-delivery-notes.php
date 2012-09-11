3ccf4e3cb5176b93eeb7b6fced5067748a2d34fd
Triggvy Gunderson <wartamau@gmail.com>
2012-09-11 15:18:06 +0200

    better order numbers

--- woocommerce-delivery-notes.php
+++ woocommerce-delivery-notes.php
@@ -321,14 +321,10 @@ if ( !function_exists( 'wcdn_order_number' ) ) {
 	function wcdn_order_number() {
 		global $wcdn;
 
-		// get custom order number as provided by the plugin
-		// http://wordpress.org/extend/plugins/woocommerce-sequential-order-numbers/
-		// if custom order number is zero, fall back to ID
-		$order_id = $wcdn->print->order_id;
-		if( $wcdn->settings->is_woocommerce_sequential_order_numbers_activated() && $wcdn->print->get_order_field( '_order_number' ) ) {
-			$order_id = $wcdn->print->get_order_field( '_order_number' );
-		}
-		echo $order_id;
+		// Trim the hash to have a clean number but still 
+		// support any filters that were applied before.
+		$order_number = ltrim($wcdn->print->get_order()->get_order_number(), '#');
+		echo $order_number;
 	}
 }
 
